996eac8ec537f094a6f593469560bde9b80a1003
vishwaaniruddh <vishwaaniruddh@gmail.com>
2024-07-25 18:57:44 +0530

    25th july 2024 backup blogs commit

---
+++ 25thJuly2024Backup/blogs/wp-content/themes/hello-elementor/header.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * This is the template that displays all of the <head> section, opens the <body> tag and adds the site's header.
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+$viewport_content = apply_filters( 'hello_elementor_viewport_content', 'width=device-width, initial-scale=1' );
+$enable_skip_link = apply_filters( 'hello_elementor_enable_skip_link', true );
+$skip_link_url = apply_filters( 'hello_elementor_skip_link_url', '#content' );
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TDG4N2Z');</script>
+	
+	
+	
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+	
+<!-- 	This is random comment  -->
+	
+	
+</head>
+	
+<body <?php body_class(); ?>>
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TDG4N2Z"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
+<?php wp_body_open(); ?>
+
+<?php if ( $enable_skip_link ) { ?>
+<a class="skip-link screen-reader-text" href="<?php echo esc_url( $skip_link_url ); ?>"><?php echo esc_html__( 'Skip to content', 'hello-elementor' ); ?></a>
+<?php } ?>
+
+<?php
+if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'header' ) ) {
+	if ( hello_elementor_display_header_footer() ) {
+		if ( did_action( 'elementor/loaded' ) && hello_header_footer_experiment_active() ) {
+			get_template_part( 'template-parts/dynamic-header' );
+		} else {
+			get_template_part( 'template-parts/header' );
+		}
+	}
+}
